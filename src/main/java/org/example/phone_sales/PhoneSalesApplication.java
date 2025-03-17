06613252a4c90ee6276577735340a5f34e07b251
bubliok <bubliok@outlook.com>
2025-03-17 17:51:41 +0200

    Dotenv

--- src/main/java/org/example/phone_sales/PhoneSalesApplication.java
+++ src/main/java/org/example/phone_sales/PhoneSalesApplication.java
@@ -2,13 +2,19 @@ package org.example.phone_sales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class PhoneSalesApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_USERNAME", Objects.requireNonNull(dotenv.get("DB_USERNAME")));
+        System.setProperty("DB_PASSWORD", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
+        System.setProperty("DB_URL", Objects.requireNonNull(dotenv.get("DB_URL")));
+
         SpringApplication.run(PhoneSalesApplication.class, args);
     }
-
 }
