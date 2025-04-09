4ce6fd970a101433c1e471629ce4b60f0c4b773d
Petyo156 <petyo156bg@gmail.com>
2025-04-10 00:07:00 +0300

    refactor RegisterRequest

--- src/main/java/bg/tu_varna/sit/usp/phone_sales/web/dto/RegisterRequest.java
+++ src/main/java/bg/tu_varna/sit/usp/phone_sales/web/dto/RegisterRequest.java
@@ -3,13 +3,14 @@ package bg.tu_varna.sit.usp.phone_sales.web.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RegisterRequest {
-    @Size(min = 3, message = "Username must be at least 3 symbols.")
-    private String username;
-
     @Size(min = 3, message = "Password must be at least 3 symbols.")
     private String password;
 
