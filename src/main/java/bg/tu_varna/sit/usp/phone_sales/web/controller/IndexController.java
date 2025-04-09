917fc08b7ee0125ec2c96c48d495d23ad1673c54
Petyo156 <petyo156bg@gmail.com>
2025-04-09 19:22:35 +0300

    add IndexController methods

--- src/main/java/bg/tu_varna/sit/usp/phone_sales/web/controller/IndexController.java
+++ src/main/java/bg/tu_varna/sit/usp/phone_sales/web/controller/IndexController.java
@@ -1,17 +1,67 @@
 package bg.tu_varna.sit.usp.phone_sales.web.controller;
 
+import bg.tu_varna.sit.usp.phone_sales.user.service.UserService;
+import bg.tu_varna.sit.usp.phone_sales.web.dto.LoginRequest;
+import bg.tu_varna.sit.usp.phone_sales.web.dto.RegisterRequest;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
-@Slf4j
 public class IndexController {
+    private final UserService userService;
+
+    @Autowired
+    public IndexController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/")
     public String index() {
 
-        return "index";
+        return "index/index";
+    }
+
+    @GetMapping("/register")
+    public ModelAndView register() {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("index/register");
+        modelAndView.addObject("registerRequest", new RegisterRequest());
+
+        return modelAndView;
+    }
+
+    @PostMapping("/register")
+    public ModelAndView register(@Valid RegisterRequest registerRequest, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ModelAndView("index/register");
+        }
+
+        userService.register(registerRequest);
+        return new ModelAndView("redirect:/login");
+    }
+
+    @GetMapping("/login")
+    public ModelAndView login(@RequestParam(value = "error", required = false) String errorParam, @Valid LoginRequest loginRequest, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index/login");
+
+        modelAndView.addObject("error", errorParam);
+        modelAndView.addObject("loginRequest", loginRequest);
+
+        if(errorParam != null || bindingResult.hasErrors()) {
+            modelAndView.addObject("errorMessage", "Incorrect username or password!");
+        }
+
+        return modelAndView;
     }
 }
