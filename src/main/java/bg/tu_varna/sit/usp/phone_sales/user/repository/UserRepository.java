8d78d063fbf56f9f995325d3aa61a030ba8630fa
Petyo156 <petyo156bg@gmail.com>
2025-04-09 19:23:14 +0300

    demonstrate how UserService register would work

--- src/main/java/bg/tu_varna/sit/usp/phone_sales/user/repository/UserRepository.java
+++ src/main/java/bg/tu_varna/sit/usp/phone_sales/user/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package bg.tu_varna.sit.usp.phone_sales.user.repository;
 
+import bg.tu_varna.sit.usp.phone_sales.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
--- src/main/java/bg/tu_varna/sit/usp/phone_sales/user/service/UserService.java
+++ src/main/java/bg/tu_varna/sit/usp/phone_sales/user/service/UserService.java
@@ -1,7 +1,22 @@
 package bg.tu_varna.sit.usp.phone_sales.user.service;
 
+import bg.tu_varna.sit.usp.phone_sales.user.model.User;
+import bg.tu_varna.sit.usp.phone_sales.user.repository.UserRepository;
+import bg.tu_varna.sit.usp.phone_sales.web.dto.RegisterRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void register(RegisterRequest registerRequest) {
+        User user = new User();
+        userRepository.save(user);
+    }
 }
