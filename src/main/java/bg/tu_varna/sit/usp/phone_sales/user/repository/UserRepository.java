f1666e27308868fe45fd957830377bca2395b50d
Petyo156 <petyo156bg@gmail.com>
2025-04-10 00:10:42 +0300

    user service logic for logging user in and registering new users

--- src/main/java/bg/tu_varna/sit/usp/phone_sales/user/repository/UserRepository.java
+++ src/main/java/bg/tu_varna/sit/usp/phone_sales/user/repository/UserRepository.java
@@ -4,8 +4,10 @@ import bg.tu_varna.sit.usp.phone_sales.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String username);
 }
