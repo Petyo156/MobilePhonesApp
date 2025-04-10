d16ca5145ad464ef4868f452dde55602a86ba4c3
Petyo156 <petyo156bg@gmail.com>
2025-04-10 21:56:10 +0300

    submit phone method refactor

--- src/main/java/bg/tu_varna/sit/usp/phone_sales/phone/service/PhoneService.java
+++ src/main/java/bg/tu_varna/sit/usp/phone_sales/phone/service/PhoneService.java
@@ -6,7 +6,7 @@ import bg.tu_varna.sit.usp.phone_sales.dimension.model.Dimension;
 import bg.tu_varna.sit.usp.phone_sales.dimension.service.DimensionService;
 import bg.tu_varna.sit.usp.phone_sales.hardware.model.Hardware;
 import bg.tu_varna.sit.usp.phone_sales.hardware.service.HardwareService;
-import bg.tu_varna.sit.usp.phone_sales.model.model.Model;
+import bg.tu_varna.sit.usp.phone_sales.model.model.PhoneModel;
 import bg.tu_varna.sit.usp.phone_sales.model.service.ModelService;
 import bg.tu_varna.sit.usp.phone_sales.operatingsystem.model.OperatingSystem;
 import bg.tu_varna.sit.usp.phone_sales.operatingsystem.service.OperatingSystemService;
@@ -14,6 +14,7 @@ import bg.tu_varna.sit.usp.phone_sales.phone.model.Phone;
 import bg.tu_varna.sit.usp.phone_sales.phone.repository.PhoneRepository;
 import bg.tu_varna.sit.usp.phone_sales.web.dto.submitphone.*;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +22,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Service
+@Slf4j
 public class PhoneService {
     private final PhoneRepository phoneRepository;
     private final DimensionService dimensionService;
@@ -47,26 +49,27 @@ public class PhoneService {
         SubmitCamera cameraInfo = submitPhoneRequest.getCamera();
         SubmitOperatingSystem operatingSystemInfo = submitPhoneRequest.getOperatingSystem();
 
-        Dimension dimension = dimensionService.submitDimension(dimensions, phone);
+        Dimension dimension = dimensionService.submitDimension(dimensions);
 
-        //TODO
-        Model model = modelService.submitBrandAndModel(brandAndModel, phone);
+        PhoneModel phoneModel = modelService.submitBrandAndModel(brandAndModel);
 
-        Hardware hardware = hardwareService.submitHardware(hardwareInfo, cameraInfo, phone);
+        Hardware hardware = hardwareService.submitHardware(hardwareInfo, cameraInfo);
 
-        OperatingSystem operatingSystem = operatingSystemService.submitOperatingSystem(operatingSystemInfo, phone);
+        OperatingSystem operatingSystem = operatingSystemService.submitOperatingSystem(operatingSystemInfo);
 
-        Phone initializePhone = initializePhone(phone, submitPhoneRequest, hardware, operatingSystem, model, dimension);
+        Phone initializedPhone = initializePhone(phone, submitPhoneRequest, hardware, operatingSystem, phoneModel, dimension);
 
-        return phoneRepository.save(initializePhone);
+        log.info("Phone initialized successfully");
+        return phoneRepository.save(initializedPhone);
     }
 
-    private Phone initializePhone(Phone phone, SubmitPhoneRequest submitPhoneRequest, Hardware hardware, OperatingSystem operatingSystem, Model model, Dimension dimension) {
+    private Phone initializePhone(Phone phone, SubmitPhoneRequest submitPhoneRequest, Hardware hardware, OperatingSystem operatingSystem, PhoneModel phoneModel, Dimension dimension) {
+        log.info("Initializing phone");
         return phone.toBuilder()
                 .dimension(dimension)
                 .hardware(hardware)
                 .operatingSystem(operatingSystem)
-                .model(model)
+                .phoneModel(phoneModel)
                 .price(BigDecimal.valueOf(submitPhoneRequest.getPrice()))
                 .quantity(submitPhoneRequest.getQuantity())
                 .releaseYear(submitPhoneRequest.getReleaseYear())
