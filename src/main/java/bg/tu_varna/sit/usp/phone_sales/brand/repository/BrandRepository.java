f3d61f461049990b16d6b8f995c216a720c4027c
Petyo156 <petyo156bg@gmail.com>
2025-04-10 21:54:39 +0300

    brand and model service logic on phone submission

--- src/main/java/bg/tu_varna/sit/usp/phone_sales/brand/repository/BrandRepository.java
+++ src/main/java/bg/tu_varna/sit/usp/phone_sales/brand/repository/BrandRepository.java
@@ -1,7 +1,13 @@
 package bg.tu_varna.sit.usp.phone_sales.brand.repository;
 
+import bg.tu_varna.sit.usp.phone_sales.brand.model.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
-public class BrandRepository {
+public interface BrandRepository extends JpaRepository<Brand, UUID> {
+    Optional<Brand> findByName(String brandName);
 }
--- src/main/java/bg/tu_varna/sit/usp/phone_sales/model/repository/ModelRepository.java
+++ src/main/java/bg/tu_varna/sit/usp/phone_sales/model/repository/ModelRepository.java
@@ -1,7 +1,14 @@
 package bg.tu_varna.sit.usp.phone_sales.model.repository;
 
+import bg.tu_varna.sit.usp.phone_sales.brand.model.Brand;
+import bg.tu_varna.sit.usp.phone_sales.model.model.PhoneModel;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
-public class ModelRepository {
+public interface ModelRepository extends JpaRepository<PhoneModel, UUID> {
+    Optional<PhoneModel> findByNameAndBrand(String modelName, Brand brand);
 }
