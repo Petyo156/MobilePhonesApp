5ee46ee0d3ea4192fef29fca0ba04be52a968b87
Petyo156 <petyo156bg@gmail.com>
2025-04-10 16:52:09 +0300

    fix repositories

--- src/main/java/bg/tu_varna/sit/usp/phone_sales/camera/repository/CameraRepository.java
+++ src/main/java/bg/tu_varna/sit/usp/phone_sales/camera/repository/CameraRepository.java
@@ -1,7 +1,11 @@
 package bg.tu_varna.sit.usp.phone_sales.camera.repository;
 
+import bg.tu_varna.sit.usp.phone_sales.camera.model.Camera;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public class CameraRepository {
+public interface CameraRepository extends JpaRepository<Camera, UUID> {
 }
