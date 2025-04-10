f8c617d30587d99c05db7fca069ff242ae792412
Petyo156 <petyo156bg@gmail.com>
2025-04-10 15:06:17 +0300

    SubmitPhoneRequest.java dto structure

--- src/main/java/bg/tu_varna/sit/usp/phone_sales/processor/repository/ProcessorRepository.java
+++
@@ -1,7 +0,0 @@
-package bg.tu_varna.sit.usp.phone_sales.processor.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class ProcessorRepository {
-}
--- src/main/java/bg/tu_varna/sit/usp/phone_sales/processor/service/ProcessorService.java
+++
@@ -1,7 +0,0 @@
-package bg.tu_varna.sit.usp.phone_sales.processor.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ProcessorService {
-}
