26fa18dfd25d7daee9103cc6005eaadde1e766f7
Petyo156 <petyo156bg@gmail.com>
2025-04-12 01:26:26 +0300

    get profile page template

--- src/main/java/bg/tu_varna/sit/usp/phone_sales/web/controller/UserController.java
+++ src/main/java/bg/tu_varna/sit/usp/phone_sales/web/controller/UserController.java
@@ -1,44 +1,35 @@
 package bg.tu_varna.sit.usp.phone_sales.web.controller;
 
 import bg.tu_varna.sit.usp.phone_sales.inventory.service.InventoryService;
-import bg.tu_varna.sit.usp.phone_sales.phone.service.PhoneService;
 import bg.tu_varna.sit.usp.phone_sales.security.AuthenticationMetadata;
 import bg.tu_varna.sit.usp.phone_sales.user.model.User;
 import bg.tu_varna.sit.usp.phone_sales.user.service.UserService;
-import bg.tu_varna.sit.usp.phone_sales.web.dto.getphoneresponse.GetPhoneResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller("/profile")
+@Controller
+@RequestMapping("/profile")
 public class UserController {
     private final UserService userService;
-    private final PhoneService phoneService;
     private final InventoryService inventoryService;
 
     @Autowired
-    public UserController(UserService userService, PhoneService phoneService, InventoryService inventoryService) {
+    public UserController(UserService userService, InventoryService inventoryService) {
         this.userService = userService;
-        this.phoneService = phoneService;
         this.inventoryService = inventoryService;
     }
 
-    @GetMapping("/phone/{slug}")
-    public ModelAndView getPhonePage(@AuthenticationPrincipal AuthenticationMetadata authenticationMetadata,
-                                     @PathVariable String slug) {
-        ModelAndView modelAndView = new ModelAndView("home/phone");
+    @GetMapping()
+    public ModelAndView getProfilePage(@AuthenticationPrincipal AuthenticationMetadata authenticationMetadata) {
+        ModelAndView modelAndView = new ModelAndView("home/profile");
 
         User user = userService.getAuthenticatedUser(authenticationMetadata);
-        GetPhoneResponse phoneResponse = phoneService.getPhoneResponseBySlug(slug);
-
         modelAndView.addObject("user", user);
-        modelAndView.addObject("phoneResponse", phoneResponse);
+//        modelAndView.addObject("orders", orders);
         return modelAndView;
     }
-
-    //TODO
-    //-getProfilePage method with past orders
 }
