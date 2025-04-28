cd61ea3649b43c8ec7c9e72cb340ccff5d3106f6
bubliok <bubliok@outlook.com>
2025-04-28 18:31:50 +0300

    checkout page updates
    fixed overall style, discount display, added google places api autocomplete and stripe card verification

--- src/main/java/bg/tu_varna/sit/usp/phone_sales/orderdetails/model/DeliveryMethod.java
+++ src/main/java/bg/tu_varna/sit/usp/phone_sales/orderdetails/model/DeliveryMethod.java
@@ -1,6 +1,13 @@
 package bg.tu_varna.sit.usp.phone_sales.orderdetails.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum DeliveryMethod {
-    STANDARD,
-    PICKUP
+    STANDARD(4.99),
+    PICKUP(0.0);
+
+    private final double price;
 }
