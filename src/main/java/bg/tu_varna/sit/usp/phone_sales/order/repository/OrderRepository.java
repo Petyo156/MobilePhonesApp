baea7b7843fa3d5499ce53ae4cf79b3a6a45c2d6
Petyo156 <petyo156bg@gmail.com>
2025-05-08 21:07:19 +0300

    order service main logic for order responses retrieval

--- src/main/java/bg/tu_varna/sit/usp/phone_sales/order/repository/OrderRepository.java
+++ src/main/java/bg/tu_varna/sit/usp/phone_sales/order/repository/OrderRepository.java
@@ -4,8 +4,11 @@ import bg.tu_varna.sit.usp.phone_sales.order.model.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Sale, UUID> {
+
+    Optional<Sale> getSaleByOrderNumber(String orderNumber);
 }
